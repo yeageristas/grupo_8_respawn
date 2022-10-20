d6724e5927ac2df42ebddb2dda0a28fa4cfa193c
sudwind <1626191+sudwind@users.noreply.github.com>
2022-10-19 21:08:53 -0300

    generic controller base implementation and repository

--- modules/respawn-backend/src/main/java/com/respawn/controllers/GenericControllerBaseImpl.java
+++ modules/respawn-backend/src/main/java/com/respawn/controllers/GenericControllerBaseImpl.java
@@ -1,4 +1,67 @@
 package com.respawn.controllers;
 
-public interface GenericControllerBaseImpl {
+import com.respawn.entities.GenericModel;
+import com.respawn.services.GenericService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public abstract class GenericControllerBaseImpl<E extends GenericModel, S extends GenericService<E>> implements GenericController<E>  {
+    public GenericControllerBaseImpl(S service) {
+        this.service = service;
+    }
+    protected  S service;
+
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(this.service.findAll());
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(String.format("{\"error\":\"%s\"}", ex.getMessage()));
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        try {
+            this.service.delete(id);
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(String.format("{\"error\":\"%s\"}", ex.getMessage()));
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> find(@PathVariable("id") Long id) {
+        try {
+            var personaOpt = this.service.findById(id);
+            if(personaOpt.isPresent()) {
+                return ResponseEntity.status(HttpStatus.OK).body(personaOpt.get());
+            } else {
+                throw new Exception(String.format("Person with id %d not found", id));
+            }
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(String.format("{\"error\":\"%s\"}", ex.getMessage()));
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody E e) {
+        try {
+            this.service.save(e);
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(String.format("{\"error\":\"%s\"}", ex.getMessage()));
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") Long id, @RequestBody E e) {
+        try {
+            this.service.update(id, e);
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(String.format("{\"error\":\"%s\"}", ex.getMessage()));
+        }
+    }
 }
--- modules/respawn-backend/src/main/java/com/respawn/repositories/GenericRepository.java
+++ modules/respawn-backend/src/main/java/com/respawn/repositories/GenericRepository.java
@@ -4,8 +4,6 @@ import com.respawn.entities.GenericModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
-import java.io.Serializable;
-
 @NoRepositoryBean
 public interface GenericRepository <E extends GenericModel> extends JpaRepository<E, Long> {
 }
