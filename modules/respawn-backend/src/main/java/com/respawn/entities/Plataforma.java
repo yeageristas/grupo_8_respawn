ee68668464c5d7ab177138c48d24ca353daa640f
Pablo <pablo.l@ensolvers.com>
2022-10-23 15:57:07 -0300

    json parser requires a constructor to deserialize

--- modules/respawn-backend/src/main/java/com/respawn/entities/Plataforma.java
+++ modules/respawn-backend/src/main/java/com/respawn/entities/Plataforma.java
@@ -1,6 +1,8 @@
 package com.respawn.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.envers.Audited;
 
@@ -9,6 +11,8 @@ import javax.persistence.Entity;
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Audited
 public class Plataforma extends GenericModel {
 
