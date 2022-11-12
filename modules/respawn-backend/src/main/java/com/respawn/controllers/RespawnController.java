f3f826f7211990a6cc2f1d120202303c4f0b6861
JulietaEsains <jesains@gmail.com>
2022-11-12 13:22:35 -0300

    pantalla de inicio preliminar

---
+++ modules/respawn-backend/src/main/java/com/respawn/controllers/RespawnController.java
@@ -0,0 +1,30 @@
+package com.respawn.controllers;
+
+import com.respawn.entities.Juego;
+import com.respawn.services.JuegoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class RespawnController {
+
+    @Autowired
+    private JuegoService juegoService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        try {
+            List<Juego> juegos = this.juegoService.findAll();
+            model.addAttribute("juegos", juegos);
+            return "views/home";
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            return "error";
+        }
+    }
+
+}
