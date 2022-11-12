d6d0b5dd3d208b15d4e6007fd15404b0e57ef5a1
sudwind <1626191+sudwind@users.noreply.github.com>
2022-11-12 17:54:08 -0300

    login

--- modules/respawn-backend/src/main/java/com/respawn/entities/Pais.java
+++ modules/respawn-backend/src/main/java/com/respawn/entities/Pais.java
@@ -1,6 +1,7 @@
 package com.respawn.entities;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.envers.Audited;
 
 import javax.persistence.Entity;
@@ -8,6 +9,11 @@ import javax.persistence.Entity;
 @Entity
 @Getter
 @Audited
+@NoArgsConstructor
 public class Pais extends GenericModel {
     String nombre;
+
+    public Pais (String nombre) {
+        this.nombre = nombre;
+    }
 }
--- modules/respawn-backend/src/main/java/com/respawn/entities/Rol.java
+++ modules/respawn-backend/src/main/java/com/respawn/entities/Rol.java
@@ -1,8 +1,20 @@
 package com.respawn.entities;
 
 
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+@Getter
 public enum Rol {
-    CLIENTE,
-    VENDEDOR,
-    ADMINISTRADOR
+    CLIENTE("CLIENTE", new SimpleGrantedAuthority("CLIENTE")),
+    VENDEDOR("VENDEDOR", new SimpleGrantedAuthority("VENDEDOR")),
+    ADMINISTRADOR("ADMINISTRADOR", new SimpleGrantedAuthority("ADMINISTRADOR"));
+
+    private final String nombre;
+    private final SimpleGrantedAuthority authority;
+
+    Rol(String nombre, SimpleGrantedAuthority authority) {
+        this.nombre = nombre;
+        this.authority = authority;
+    }
 }
