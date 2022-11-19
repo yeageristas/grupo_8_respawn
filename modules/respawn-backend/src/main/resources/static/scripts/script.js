36ef57b39d94f3bdf066a9cbd65ba0e05d34310e
sudwind <1626191+sudwind@users.noreply.github.com>
2022-11-19 18:05:34 -0300

    rework logout handler

--- modules/respawn-backend/src/main/resources/static/scripts/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('login__logout').addEventListener("click", (e) => {
-     e.preventDefault();
-     window.location.href = window.location.href + "../../../logout";
- })
-
- console.log('scripts loaded');
