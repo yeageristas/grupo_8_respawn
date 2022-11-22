6242e45e35c6e3acd0c3d0e62d2e8864d3d719be
ArielSantos01 <arielg.santos21@gmail.com>
2022-11-22 05:32:28 -0300

    detalles del pedido

--- modules/respawn-backend/src/main/java/com/respawn/controllers/RespawnController.java
+++ modules/respawn-backend/src/main/java/com/respawn/controllers/RespawnController.java
@@ -11,11 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Controller
 public class RespawnController {
@@ -358,4 +356,22 @@ public class RespawnController {
             return "error";
         }
     }
+
+    @GetMapping("/detallePedido")
+    public String detallePedido(Model model){
+            final String userEmail = authenticationService.getUserEmail();
+            var usuario = usuarioService.findByEmail(userEmail);
+            var pedido = pedidoService.findPedidoByUser(usuario.get().getId());
+
+            model.addAttribute("detalles",  pedido.get().getListaPedidoDetalle());
+            model.addAttribute("pedido", pedido.get());
+            model.addAttribute("usuario", usuario.get());
+            model.addAttribute("pais", usuario.get().getPais().getNombre());
+            var total = pedido.get().calcularMontoTotal();
+            model.addAttribute("total", total);
+
+        return "views/detallesDelPedido";
+    }
+
+
 }
